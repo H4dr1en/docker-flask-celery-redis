e81b480fb9c66134ec4055a33e1c31f77c94eef0
H4dr1en <h4dr1en@pm.me>
2019-01-22 06:12:54 -0500

    Splitting containers in different images Celery/Gunicorn

--- api/app.py
+++ api/app.py
@@ -1,22 +1,49 @@
-from flask import Flask
-from flask import url_for
+from flask import Flask, jsonify, request, url_for
 from worker import celery
 import celery.states as states
 
 app = Flask(__name__)
 
 
-@app.route('/add/<int:param1>/<int:param2>')
-def add(param1: int, param2: int) -> str:
-    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
-    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
-    return response
+@app.route('/py/checkAuth', methods=['POST'])
+def checkAuth():
+
+    req = request.get_json()
+
+    for attr in ["uid", "abs", "ords", "time"]:
+        if attr not in req.keys():
+            return 
+
+    async_task = celery.send_task('tasks.process', args=[req], kwargs={})
+
+    return jsonify({"taskid":async_task.id})
 
 
-@app.route('/check/<string:task_id>')
-def check_task(task_id: str) -> str:
-    res = celery.AsyncResult(task_id)
-    if res.state == states.PENDING:
-        return res.state
-    else:
-        return str(res.result)
+@app.route('/py/status/<task_id>', methods=['GET'])
+def checkStatus(task_id):
+
+    task = celery.AsyncResult(task_id)
+
+    status = task.info.get('output')
+
+    if status != 'FAILURE':
+        response = {
+            'state': status,
+            'client': task.info.get('client')
+        }
+        
+        if 'isAuthValid' in task.info:
+            response['isAuthValid'] = task.info.get('isAuthValid', False)
+    else: 
+        # something went wrong in the background job
+        response = {
+            'state': 'FAILURE',     
+            'client': task.info.get('client'),
+            'msg': task.info.get('msg', ''),
+            'isAuthValid': False # Invalid authentification
+        }
+
+    if status != 'PROGRESS':    # If task is finished, clear results
+        task.forget()
+
+    return jsonify(response)
